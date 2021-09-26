687836ef9d330dafc5415b9306856a5723ba978c
sh-study <0604seohee@gmail.com>
2021-09-27 08:02:34 +0900

    commit

--- index-js.js
+++ index-js.js
@@ -1,13 +1,7 @@
-const fs = require("fs");
-
 window.onload = () => {
-    const files = fs.readdirSync("./_posts");
-    for (const file of files) {
-        const newSec = document.createElement("section");
-        const newText = document.createTextNode(file);
-        newSec.appendChild(newText);
-        const div = document.getElementById("list");
-        div.appendChild(newSec);
-    }
-    
+    const newSec = document.createElement("section");
+    const newText = document.createTextNode(file);
+    newSec.appendChild(newText);
+    const div = document.getElementById("list");
+    div.appendChild(newSec);
 };
