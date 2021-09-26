fce9a832557d985d2505783bba2f0c59afffd408
sh-study <0604seohee@gmail.com>
2021-09-27 08:03:00 +0900

    commit

--- index-js.js
+++ index-js.js
@@ -1,6 +1,6 @@
 window.onload = () => {
     const newSec = document.createElement("section");
-    const newText = document.createTextNode(file);
+    const newText = document.createTextNode("something");
     newSec.appendChild(newText);
     const div = document.getElementById("list");
     div.appendChild(newSec);
