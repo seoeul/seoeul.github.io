9c1c466aadd83116d0dd644bd4a366d08b00983e
sh-study <0604seohee@gmail.com>
2021-09-27 07:13:26 +0900

    commit

--- index-js.js
+++ index-js.js
@@ -4,7 +4,7 @@ window.onload = () => {
     const files = fs.readdirSync("./_posts");
     for (const file of files) {
         const newSec = document.createElement("section");
-        const newText = document.createTextNode("hi");
+        const newText = document.createTextNode(file);
         newSec.appendChild(newText);
         const div = document.getElementById("list");
         div.appendChild(newSec);
