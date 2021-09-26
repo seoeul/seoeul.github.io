4e3efd0e76ae10e1db447324b6f85b17195f47e6
sh-study <0604seohee@gmail.com>
2021-09-26 16:03:23 +0900

    commit

--- index-js.js
+++ index-js.js
@@ -1,22 +1,7 @@
-const fs = require("fs");
-
-window.addEventListener("load", showList);
-
-function showList() {
-    // const title = "";
-    // const f = fs.readdirSync("./_posts");
-    // for (const file of f) {
-    //     const newSec = document.createElement("section");
-    //     const name = file.split('.');
-    //     const list = name[0].split('_');
-    //     title = list[0]
-    //     const newText = document.createTextNode(title);
-    //     newSec.appendChild(newText);
-    //     const div = document.getElementById("list");
-    //     div.appendChild(newSec);
-    // }
+window.addEventListener("load", event => {
     const newSec = document.createElement("section");
     const newText = document.createTextNode("hi");
     newSec.appendChild(newText);
-    document.body.appendChild(newSec);
-}
+    const div = document.getElementById("list");
+    div.appendChild(newSec);
+});
