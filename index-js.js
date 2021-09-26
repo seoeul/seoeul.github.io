c3d215a46d6e53cbc0ea7e7b80aa108d66a71984
sh-study <0604seohee@gmail.com>
2021-09-27 07:07:11 +0900

    commit

--- index-js.js
+++ index-js.js
@@ -1,7 +1,10 @@
-window.addEventListener("load", event => {
+const fs = require("fs");
+
+window.onload = () => {
+    console.log(fs.readdirSync("./_posts"));
     const newSec = document.createElement("section");
     const newText = document.createTextNode("hi");
     newSec.appendChild(newText);
     const div = document.getElementById("list");
     div.appendChild(newSec);
-});
+};
