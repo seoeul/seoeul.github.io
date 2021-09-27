c47c709bdfd7b67f48e5815d02a067e509973318
sh-study <0604seohee@gmail.com>
2021-09-27 18:03:14 +0900

    update all

--- index-js.js
+++ index-js.js
@@ -2,6 +2,6 @@ window.onload = () => {
     const newSec = document.createElement("section");
     const newText = document.createTextNode("something");
     newSec.appendChild(newText);
-    const div = document.getElementById("list");
+    const div = document.getElementById("main");
     div.appendChild(newSec);
 };
