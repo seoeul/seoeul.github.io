374147b8761bb6ddc81dd9196c17b26cb4ad9e34
sh-study <0604seohee@gmail.com>
2021-09-27 20:32:07 +0900

    bug fixed

--- index-js.js
+++ index-js.js
@@ -2,6 +2,6 @@ window.onload = () => {
     const newSec = document.createElement("section");
     const newText = document.createTextNode("something");
     newSec.appendChild(newText);
-    const div = document.getElementById("main");
+    const div = document.getElementsByClassName("main")[0];
     div.appendChild(newSec);
 };
