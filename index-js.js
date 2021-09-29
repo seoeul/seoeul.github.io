9919b08351568ff28402940704a609b75514c2c2
sh-study <0604seohee@gmail.com>
2021-09-29 23:18:01 +0900

    full update

--- index-js.js
+++
@@ -1,7 +0,0 @@
-window.onload = () => {
-    const newSec = document.createElement("section");
-    const newText = document.createTextNode("something");
-    newSec.appendChild(newText);
-    const div = document.getElementsByClassName("main")[0];
-    div.appendChild(newSec);
-};
