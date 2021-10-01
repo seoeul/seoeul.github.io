32a53ba77f4843089328883737f53bdd9eed2816
sh-study <0604seohee@gmail.com>
2021-10-01 21:47:52 +0900

    massive full update

--- home.js
+++ home.js
@@ -1,1 +1,33 @@
-let title = document.querySelector(".title");
+const blogs = document.querySelector(".blogs");
+let title = ["Making_blog", "우애애애애애ㅐㅇㅇ", "우웅우우우우우ㅜ우우ㅜ"];
+
+const create = (n) => {
+
+    const blogtitle = document.createElement("h1");
+    blogtitle.setAttribute("class", "blog-title");
+    blogtitle.innerHTML = title[n].replace("_", " ");
+
+    const blogoverview = document.createElement("p");
+    blogoverview.setAttribute("class", "blog-overview");
+    blogoverview.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt incidunt fugiat quos porro repellat harum. "
+
+    const img = document.createElement("img");
+    img.setAttribute("src", `./img/${title[n]}.png`);
+    img.setAttribute("class", "blog-img");
+
+    const blogcard = document.createElement("div");
+    blogcard.setAttribute("class", "blog-card");
+    blogcard.appendChild(img);
+    blogcard.appendChild(blogtitle);
+    blogcard.appendChild(blogoverview);
+
+    const bloglink = document.createElement("a");
+    bloglink.setAttribute("href", `./posts/${title[n]}.html`);
+    bloglink.appendChild(blogcard);
+    blogs.appendChild(bloglink);
+
+};
+
+for (i = 0; i < title.length; i++) {
+    create(i);
+}
