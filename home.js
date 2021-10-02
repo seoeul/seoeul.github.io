9750b0655c7fa2c77908c30e07a976bc4a69ff64
sh-study <0604seohee@gmail.com>
2021-10-02 16:13:26 +0900

    create post loading

--- home.js
+++ home.js
@@ -1,33 +1,57 @@
-const blogs = document.querySelector(".blogs");
-let title = ["Making_blog", "CSS+html_frontend", "우웅우우우우우ㅜ우우ㅜ"];
+let category = [];
+let title = [];
+let overview = [];
 
-const create = (n) => {
+$.getJSON("./posts.json", (data) => {
+    $.each(data, (key, value) => {
+        category.push(key);
+        $.each(value, (key, value) => {
+            title.push(value.title);
+            overview.push(value.overview);
+        });
+    });
+    createBlogcard(title.length);
+});
 
-    const blogtitle = document.createElement("h1");
-    blogtitle.setAttribute("class", "blog-title");
-    blogtitle.innerHTML = title[n].replace("_", " ");
+const createBlogcard = (t) => {
 
-    const blogoverview = document.createElement("p");
-    blogoverview.setAttribute("class", "blog-overview");
-    blogoverview.innerHTML = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt incidunt fugiat quos porro repellat harum. "
+    const section = document.createElement("section");
+    section.setAttribute("class", "blogs");
 
-    const img = document.createElement("img");
-    img.setAttribute("src", `./img/${title[n]}.png`);
-    img.setAttribute("class", "blog-img");
+    for (i = t-1; i > -1; i--) {
 
-    const blogcard = document.createElement("div");
-    blogcard.setAttribute("class", "blog-card");
-    blogcard.appendChild(img);
-    blogcard.appendChild(blogtitle);
-    blogcard.appendChild(blogoverview);
+        const blogtitle = document.createElement("h3");
+        blogtitle.setAttribute("class", "blog-title");
+        blogtitle.innerHTML = title[i].replaceAll("_", " ");
 
-    const bloglink = document.createElement("a");
-    bloglink.setAttribute("href", `./posts/${title[n]}.html`);
-    bloglink.appendChild(blogcard);
-    blogs.appendChild(bloglink);
+        const blogoverview = document.createElement("p");
+        blogoverview.setAttribute("class", "blog-overview");
+        blogoverview.innerHTML = overview[i];
+
+        const img = document.createElement("img");
+        img.setAttribute("src", `./img/${title[i]}.png`);
+        img.setAttribute("class", "blog-img");
+
+        const blogcard = document.createElement("div");
+        blogcard.setAttribute("class", "blog-card");
+        blogcard.appendChild(img);
+        blogcard.appendChild(blogtitle);
+        blogcard.appendChild(blogoverview);
+
+        const bloglink = document.createElement("a");
+        bloglink.setAttribute("href", `./posts/${title[i]}.html`);
+        bloglink.appendChild(blogcard);
+
+        section.appendChild(bloglink);
+
+    }
+    
+    const label = document.createElement("h2");
+    label.setAttribute("class", "category");
+    label.innerHTML = "Latest";
+
+    const main = document.querySelector(".main");
+    main.appendChild(label);
+    main.appendChild(section);
 
 };
-
-for (i = 0; i < title.length; i++) {
-    create(i);
-}
