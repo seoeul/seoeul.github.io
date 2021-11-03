082ba053715b10f03d997cd5b6b1011ee00e51e4
sh-study <0604seohee@gmail.com>
2021-11-03 22:45:09 +0900

    effects update

--- posts.js
+++ posts.js
@@ -1,31 +1,53 @@
-const cursor = document.querySelector(".cursor")
+const cursorEffect = () => {
+    const cursor = document.querySelector(".cursor")
 
-document.addEventListener("mousemove", e => {
-    const mouseX = e.clientX + "px";
-    const mouseY = e.clientY + "px";
+    document.addEventListener("mousemove", e => {
+        const mouseX = e.clientX + "px";
+        const mouseY = e.clientY + "px";
 
-    cursor.style.left = mouseX;
-    cursor.style.top = mouseY;
-});
-
-const nav_a = document.querySelectorAll(".nav-a");
-
-nav_a.forEach(link => {
-    link.addEventListener("mouseover", () => {
-        cursor.classList.add("cursor-black-grow");
+        cursor.style.left = mouseX;
+        cursor.style.top = mouseY;
     });
-    link.addEventListener("mouseleave", () => {
-        cursor.classList.remove("cursor-black-grow");
-    });
-});
 
-const blog_link = document.querySelectorAll(".blog-link");
+    let cursor_black_grow = [];
+    document.querySelectorAll(".black-grow").forEach(each => {
+        cursor_black_grow.push(each);
+    });
 
-blog_link.forEach(link => {
-    link.addEventListener("mouseover", () => {
-        cursor.classList.add("cursor-white-grow");
+    cursor_black_grow.forEach(link => {
+        link.addEventListener("mouseover", () => {
+            cursor.classList.add("cursor-black-grow");
+        });
+        link.addEventListener("mouseleave", () => {
+            cursor.classList.remove("cursor-black-grow");
+        });
     });
-    link.addEventListener("mouseleave", () => {
-        cursor.classList.remove("cursor-white-grow");
+
+    let cursor_white_grow = [];
+    document.querySelectorAll(".white-grow").forEach(each => {
+        cursor_white_grow.push(each);
     });
-});
+
+    cursor_white_grow.forEach(link => {
+        link.addEventListener("mouseover", () => {
+            cursor.classList.add("cursor-white-grow");
+        });
+        link.addEventListener("mouseleave", () => {
+            cursor.classList.remove("cursor-white-grow");
+        });
+    });
+};
+
+const headerEffect = () => {
+    const header = document.querySelector(".header");
+    window.addEventListener("scroll", () => {
+        if (scrollY > 0) {
+            header.classList.add("scroll-header");
+        } else {
+            header.classList.remove("scroll-header");
+        }
+    });
+};
+
+cursorEffect();
+headerEffect();
